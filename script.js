db953249e959cacb6426f70460910d74d5d8ecee
Lara Gaete <largaete@gmail.com>
2024-07-09 20:09:14 -0300

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+let montoCompra = 150 
+let descuento = 0
+if(montoCompra > 100){
+    descuento= montoCompra * 0.1
+
+}
+console.log("Monto de la compra: $" + montoCompra)
+console.log("Descuento aplicado: $" + descuento)
+
+let cantidadIteraciones = 5
+for (let i=1; i <= cantidadIteraciones; i++)
+    console.log( "Iteracion numero: " + i)
+console.log("Ciclo finalizado")
+
+function calcularPrecioFinal(precio,descuento){
+    let descuentoDecimal=descuento/100
+    let montoDescuento= precio * descuentoDecimal
+    let precioFinal= precio - montoDescuento
+    return precioFinal.toFixed(2)
+}
+
+let precioProducto= 
+parseFloat(prompt("Ingrese el precio del producto "))
+
+let porcentajeDescuento = 
+parseFloat(prompt("Ingrese porcentaje de descuento"))
+
+let precioFinal = 
+calcularPrecioFinal(precioProducto,porcentajeDescuento)
+console.log(`El precio final despues del descuento es : $${precioFinal}`)
